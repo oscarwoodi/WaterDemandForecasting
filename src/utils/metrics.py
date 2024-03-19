7d5d28bcfcfb99a0ef8fc7e2963c85dc0c47e2f1
Oscar Wooding <Oscarwooding@gmail.com>
2024-03-19 15:00:23 +0000

    add data imputing method comparison

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,21 +1,32 @@
 import pandas as pd
 import numpy as np
     
-def mape(predictions, actuals):
+def mape(actuals, predictions):
     """Mean absolute percentage error"""
-    return ((predictions - actuals).abs() / actuals).mean()
+    return (abs(predictions - actuals) / actuals).mean()
 
-def rmse(predictions, actuals):
+def rmse(actuals, predictions):
     """Root mean squared error"""
     return (((predictions - actuals)**2).mean())**0.5
 
-def mae(predictions, actuals):
+def mae(actuals, predictions):
     """Mean absolute error"""
-    return (predictions - actuals).mean()
+    return abs(predictions - actuals).mean()
 
-def r(prediction, actuals): 
+def r(actuals, predictions): 
     """r-squared error"""
     ssreg = numpy.sum((actuals - prediction)**2)
     sstot = numpy.sum((actuals - actuals.mean())**2)
     
     return 1 - ssreg / sstot
+
+#Evaluate all
+def scores(test,pred):
+    score_rmse = rmse(test,pred)
+    score_mape = mape(test,pred)
+    score_mae = mae(test,pred)
+    #score_nse = nse(test,pred)
+    df = pd.DataFrame({'indicator':['RMSE','MAPE','MAE'],
+          'value':[score_rmse,score_mape,score_mae]})
+    df.set_index('indicator',inplace=True)
+    return (df)
