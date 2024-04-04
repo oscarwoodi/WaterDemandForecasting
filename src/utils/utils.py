f482478bdb2e8ade6360b71ca804f38559ab8259
Oscar Wooding <Oscarwooding@gmail.com>
2024-04-04 09:15:47 +0100

    updated models

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
 
 def plot_results(forecast, test): 
     """
@@ -61,7 +63,7 @@ def extract_features(df):
 
     for i in df.index:
         if str(i)[:10] in event_day:
-            df['event_day'][i] = 0
+            df['event_day'][i] = 1
 
     # add variable for weekend days
     for i in df.index:
@@ -78,3 +80,104 @@ def extract_features(df):
 
     return df
 
+def virtual_data(df, p): 
+    """
+    inserts virtual data points into input data to reduce non-linearity. 
+
+    df - input data including several dmas
+    p - number of virtual data points to include between consecutive real values
+    """
+
+    copy = df.copy()
+    freq = 60/(p+1)
+    resampled = copy.resample(str(freq)+'min')
+    interpolated = resampled.interpolate(method='linear')   
+
+    return interpolated        
+
+def classify_values(df, k):
+    """
+    classify each data point for dma into k classes. 
+
+    df - input data including several dmas
+    k - number of classes to use
+    """
+
+    # Initialize an empty DataFrame to store the classified values
+    classified_df = pd.DataFrame(index=df.index, columns=df.columns)
+
+    # Loop through each column in the DataFrame
+    for col in df.columns:
+        # Extract column values
+        column_values = df[col].values.reshape(-1, 1)
+
+        # Perform k-means clustering
+        kmeans = KMeans(n_clusters=k, random_state=0)
+        kmeans.fit(column_values)
+
+        # Get cluster centers
+        cluster_centers = kmeans.cluster_centers_
+
+        # Classify each value into k classes based on the cluster centers
+        classified_values = np.argmin(np.abs(column_values - cluster_centers.T), axis=1)
+
+        # Store the classified values in the classified DataFrame
+        classified_df[col] = classified_values
+
+    return classified_df
+
+def fragment_dma(df, dma, n_steps_present=30, n_steps_recent=30, n_steps_distant=30, n_steps_out=24): 
+    """
+    split data into fragments to be used in neural network. 
+
+    df - input data including several dmas
+    dma - dma to produce result for
+    n_steps_present - number of steps backward from forecast at t
+    n_steps_recent - number of steps included from same time in previous day
+    n_steps_distant - number of steps included from same time in 4th day prior
+    n_steps_out - number of steps included in the forecast
+    """
+    sequence = df[dma].values.astype('float32')
+    dow = df['day_arr'].values
+    holiday = df['official_holiday'].values.astype('int')
+    time = df.index.strftime("%H%M").astype('int')/2400
+    
+    x, y = [],[]
+    #n_steps_in = n_steps_present + n_steps_recent + n_steps_distant
+    for i in range(len(sequence)):
+        if i+n_steps_present+n_steps_out > len(sequence):
+            #Prediction out of sequence
+            break
+        
+        if ((i-96+int(n_steps_recent/2)<0)| (i-96*4+int(n_steps_distant/2)<0)):
+        #Past data not available
+            continue
+
+        range_present = range(i,i+n_steps_present)
+        range_recent = range(i,i+n_steps_recent)
+        range_distant = range(i,i+n_steps_distant)
+
+        range_predict = range(i+n_steps_present,i+n_steps_present+n_steps_out)
+        
+        r = range_present
+        input_present = sequence[r] #Data 5 timesteps before
+        input_present_e = np.concatenate((dow[i],holiday[i],time[i]),axis=None)
+        #input_present_e = np.concatenate((np.concatenate(dow[r]),holiday[r],time[r]),axis=None)
+
+        r = [r-96+int(n_steps_recent/2) for r in range_recent]
+        input_recent =  sequence[r] #Data one day before
+        input_recent_e = np.concatenate((np.concatenate(dow[r]),holiday[r],time[r]),axis=None)
+
+        r = [r-96*4+int(n_steps_distant/2) for r in range_distant]
+        input_distant =  sequence[r] #Data 4days before
+        input_distant_e = np.concatenate((np.concatenate(dow[r]),holiday[r],time[r]),axis=None)
+
+        input = np.concatenate((input_present, input_recent, input_distant),axis=None)
+        input = np.concatenate((input, input_present_e),axis=None)
+        #input = np.concatenate((input, input_present_e, input_recent_e, input_distant_e),axis=None)
+        output = sequence[range_predict]
+
+        x.append(input)
+        y.append(output)
+
+    return np.squeeze(np.array(x)), np.squeeze(np.array(y))
